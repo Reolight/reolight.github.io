53b1b6a611a7d4a48436f253cd724d0e23b18283
Melnikau <reolightmiene@gmail.com>
2024-10-29 18:33:33 +0300

    add test for shiftable length detection

---
+++ src/test/shiftableBlockLength.test.ts
@@ -0,0 +1,50 @@
+import { expect, test } from "vitest";
+import { maskSynthetizer, nonReqMask, nonReqMaskWithLiterl } from "./common";
+
+test("Определить длину сдвигаемого блока со значением '1   ' с позиции 0", () => {
+    maskSynthetizer.generate(nonReqMask);
+    maskSynthetizer.putSymbols("1", 0);
+
+    const actual = maskSynthetizer["getShiftableBlockLength"](0);
+
+    expect(actual).toBe(1);
+});
+
+test("Определить длину сдвигаемого блока со значением ' 11 ' с позиции 0", () => {
+    maskSynthetizer.generate(nonReqMask);
+    maskSynthetizer.putSymbols("11", 1);
+
+    const actual = maskSynthetizer["getShiftableBlockLength"](1);
+
+    expect(actual).toBe(2);
+});
+
+test("Определить длину сдвигаемого блока со значением '1 1 ' с позиции 0", () => {
+    maskSynthetizer.generate(nonReqMask);
+    maskSynthetizer.putSymbols("1", 0);
+    maskSynthetizer.putSymbols("1", 2);
+
+    const actual = maskSynthetizer["getShiftableBlockLength"](0);
+
+    expect(actual).toBe(3);
+});
+
+test("Определить длину сдвигаемого блока со значением '1 .  ' с позиции 0", () => {
+    maskSynthetizer.generate(nonReqMaskWithLiterl);
+    maskSynthetizer.putSymbols("1", 0);
+
+    const actual = maskSynthetizer["getShiftableBlockLength"](0);
+
+    expect(actual).toBe(1);
+});
+
+
+test("Определить длину сдвигаемого блока со значением '1 .1 ' с позиции 0", () => {
+    maskSynthetizer.generate(nonReqMaskWithLiterl);
+    maskSynthetizer.putSymbols("1", 0);
+    maskSynthetizer.putSymbols("1", 3);
+
+    const actual = maskSynthetizer["getShiftableBlockLength"](0);
+
+    expect(actual).toBe(3);
+});
