4fa34dba7f71829f4a7bdac1494add7214b12ff3
Melnikau <reolightmiene@gmail.com>
2024-11-01 10:51:03 +0300

    add date time localized literals

--- src/MaskedEngine/definitions.ts
+++ src/MaskedEngine/definitions.ts
@@ -7,6 +7,10 @@ import { getCurrencyCode } from "./utils";
 
 export const EmptyAction: ActionProcessor = (char) => char;
 
+const DUMMY_DATE = new Date();
+const DUMMY_FRAC = 0.1;
+const DUMMY_THOUSAND = 1_000;
+
 const maskCharactersDefinitions: MaskedCharacterDefscontainers = {
     placeholders: {
         "0": {
@@ -71,17 +75,24 @@ const maskCharactersDefinitions: MaskedCharacterDefscontainers = {
         },
     },
     localizedLiterals: {
+        "/": {
+            visibleAs() {
+                return Intl.DateTimeFormat(navigator.language)
+                    .formatToParts(DUMMY_DATE)
+                    .find((part) => part.type === "literal")!.value;
+            },
+        },
         ".": {
             visibleAs() {
                 return Intl.NumberFormat(navigator.language)
-                    .formatToParts(0.1)
+                    .formatToParts(DUMMY_FRAC)
                     .find((part) => part.type === "decimal")!.value;
             },
         },
         ",": {
             visibleAs() {
                 return Intl.NumberFormat(navigator.language)
-                    .formatToParts(1_000)
+                    .formatToParts(DUMMY_THOUSAND)
                     .find((part) => part.type === "group")!.value;
             },
         },
@@ -91,8 +102,24 @@ const maskCharactersDefinitions: MaskedCharacterDefscontainers = {
                     style: "currency",
                     currency: getCurrencyCode(navigator.language),
                 })
-                    .formatToParts(0)
+                    .formatToParts(DUMMY_FRAC)
                     .find((part) => part.type === "currency")!.value;
+            },
+        },
+        ":": {
+            visibleAs() {
+                const parts = Intl.DateTimeFormat(navigator.language, {
+                    timeStyle: "short",
+                    dateStyle: "short",
+                }).formatToParts(DUMMY_DATE);
+
+                for (let i = parts.length - 1; i > 0; i -= 1) {
+                    if (parts[i].type === "literal") {
+                        return parts[i].value;
+                    }
+                }
+
+                return ":";
             },
         },
     },
